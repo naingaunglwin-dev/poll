c542c5f1cd052a30d100d213f8cdf575a7002efe
naingaunglwin-dev <naingaunglwin.wd@gmail.com>
2024-11-17 20:49:46 +0630

    Implement Home page

---
+++ app/Views/home.php
@@ -0,0 +1,25 @@
+<?php $this->extend('template') ?>
+
+<?php $this->section('content') ?>
+
+<div class="h-[90vh] grid place-content-center">
+    <div class="p-2 sm:p-0">
+        <div class="relative bg-white shadow-md border border-black border-opacity-10 rounded-lg p-10 sm:p-32 space-y-10">
+            <div class="absolute z-40 inset-x-0 -top-40 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+                <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+            </div>
+            <h2 class="text-center">Let's create a poll and share with your friendsss!</h2>
+            <a href="<?php echo base_url('setup') ?>" class="relative z-50 no-underline flex justify-center items-center gap-4 py-2 border border-black border-opacity-20 rounded-lg hover:bg-sky-50">
+                <svg class="w-4 h-4 text-sky-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/>
+                </svg>
+                Create poll
+            </a>
+            <div class="absolute z-40 inset-x-0 top-[calc(79%-30rem)] transform-gpu overflow-hidden blur-3xl sm:top-[calc(79%-30rem)]" aria-hidden="true">
+                <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php $this->endSection() ?>
